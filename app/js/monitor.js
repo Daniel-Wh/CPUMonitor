b837e45f409e4f87680083a8b106216013675bb4
Daniel Whitney <whitneyd@southwestern.edu>
2020-07-30 10:00:38 -0500

    updated ui with static sys info

---
+++ app/js/monitor.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const osu = require("node-os-utils");
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+// set model
+
+document.getElementById("cpu-model").innerText = cpu.model();
+
+// Computer name
+
+document.getElementById("comp-name").innerText = os.hostname();
+
+// OS
+document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
+
+// Total mem
+mem.info().then((info) => {
+  document.getElementById("mem-total").innerText = `${info.totalMemMb} GB`;
+});
